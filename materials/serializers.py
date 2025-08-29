2455fe1b42032ca214d54b3302a9672d6b90ec0b
vladosiysyi <chiklow@mail.ru>
2025-08-29 16:35:09 +0300

    fixes commit

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
 from .models import Course, Lesson
 
-class LessonSerializer(serializers.ModelSerializer):
+
+#сериализатор урока (для платежей и детальных выводов)
+class LessonDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ['id', 'title', 'description', 'preview', 'video_url']
 
+
+#сериализатор урока (для интеграции в курс)
+class LessonShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['id', 'title']
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many=True, read_only=True)  # Вложенный сериализатор
+    lessons = LessonShortSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics, filters
+from rest_framework import generics, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Payment
 from .serializers import PaymentSerializer
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+User = get_user_model()
 
 class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.select_related('user', 'course', 'lesson').all()
@@ -11,3 +15,18 @@ class PaymentListAPIView(generics.ListAPIView):
     filterset_fields = ['course', 'lesson', 'payment_method']
     ordering_fields = ['date']
     ordering = ['-date']
+
+class UserListView(generics.ListAPIView):
+    """Список всех пользователей (для админов)"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    """Просмотр и редактирование своего профиля"""
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
